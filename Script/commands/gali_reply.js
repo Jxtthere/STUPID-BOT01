c472c2816dcde55a3eb6dbf2a37de87f6899b6c1
Jxtthere <jassajattt01@gmail.com>
2025-08-24 10:01:56 +0530

    Update gali_reply.js

--- Script/commands/gali_reply.js
+++ Script/commands/gali_reply.js
@@ -5,7 +5,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
 	description: "no prefix",
-	commandCategory: "no prefix",
+	commandCategory: "/",
 	usages: "abal",
     cooldowns: 5, 
 };
