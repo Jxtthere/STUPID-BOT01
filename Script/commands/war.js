374163302e7c821c99fc487ab7c564717f89d328
Jxtthere <jassajattt01@gmail.com>
2025-08-26 21:36:27 +0530

    Update war.js

--- Script/commands/war.js
+++ Script/commands/war.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "war",
     version: "1.0.0",
-    hasPermssion: 2,
+    hasPermssion: 1,
     credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "War in group boxchat",
     commandCategory: "group",
